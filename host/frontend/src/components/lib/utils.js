5cc7bd4ad143142d9b027abe4de8f9055263800e
sudoping01 <sudoping01@gmail.com>
2025-07-04 00:20:03 +0000

    cleanup

--- host/frontend/src/components/lib/utils.js
+++
@@ -1,7 +0,0 @@
-// frontend/src/lib/utils.js
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-}
--- host/setup.sh
+++
@@ -1,5 +0,0 @@
-# Deploy updates
-./deploy.sh
-
-# Monitor logs
-docker-compose -f docker-compose.prod.yaml logs -f
